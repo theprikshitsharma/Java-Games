0b9a6b5e594f97476e6538ca578f48c8cf605035
theprikshitsharma <theprikshitsharma@gmail.com>
2024-05-21 16:42:32 +0530

    Delete WhacAMole directory

--- WhacAMole/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        WhacAChicken a = new WhacAChicken();
-    }
-}
-/*My first solo project. I tried to keep it as simple as possible. I haven't seen any bugs so far. I wrote an explanation for every line of code alongside it, so refer to it if you have any doubts.*/
